30ed3fec5c4e65f539df8cb1c70d0eb0e92be80a
dimayasin <dyasin@gmail.com>
2018-02-20 15:01:02 -0600

    Ninja Gold

--- python_stack/django_projects/ninja_gold/apps/ninja_app/urls.py
+++ python_stack/django_projects/ninja_gold/apps/ninja_app/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
     url(r'^$', views.index),
-    # url(r'^process$', views.process)        
+    url(r'^process_money$', views.process_money),
+    url(r'^reset$', views.reset)        
 ]
--- python_stack/django_projects/survey_form/apps/surveys/views.py
+++ python_stack/django_projects/survey_form/apps/surveys/views.py
@@ -4,26 +4,29 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
-
-
 def index(request):
-    if not 'gold' in request.session:
-        request.session['gold'] = 0
-        request.session['msg'] = ""
+  # if not 'number' in session:
+  #       session['name'] = ""
+  #       session['location'] = ""
+  #       session['language'] = ""
+  #       session['comment'] = ""
 
-    return render(request, 'ninja/index.html')
+  if not 'counter' in request.session:
+    request.session['counter'] = 0
 
+ 
+  return render(request,'survey/index.html')
 
 def process(request):
-    timer = request.session['counter']
-    timer += 1
-    request.session['counter'] = timer
-    context = {
-        'name': request.POST['name'],
-        'location': request.POST['location'],
-        'language': request.POST['language'],
-        'comment': request.POST['comment'],
-        'counter': request.session['counter']
+  timer = request.session['counter'] 
+  timer += 1
+  request.session['counter'] = timer
+  context = {
+  'name': request.POST['name'],
+  'location':request.POST['location'],
+  'language':request.POST['language'],
+  'comment':request.POST['comment'],
+  'counter':request.session['counter']
     }
 
-    return render(request, "survey/results.html", context)
+  return render(request,"survey/results.html", context)
